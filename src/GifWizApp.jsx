479e023ba12f90bc77f2ade6c72ca10536462074
Alvaro Marenco <alvaromarencom@gmail.com>
2024-10-23 14:35:27 -0600

    Creando una lista de categorias

--- src/GifWizApp.jsx
+++ src/GifWizApp.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 
 export const GifWizApp = () => {
+
+    const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball'])
+
   return (
     <>
+        {/* titulo */}
       <h1>GifWizApp</h1>
+
+
+        {/* imput */}
+
+
+        {/* listado de Gifs */}
+        <ol>
+            {
+            categories.map( category => {
+                return <li key={category}>{category}</li>
+            })
+            }
+            {/* <li>ABC</li> */}
+        </ol>
+
+            {/* Gif items */}
     </>
   )
 }
