2bd4a2c445c3bf4009f91fb25de3855222b9a4eb
Alvaro Marenco <alvaromarencom@gmail.com>
2024-10-28 13:36:39 -0600

    Pruebas en el componente AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 
 
@@ -31,3 +32,6 @@ export const AddCategory = ({onNewCategory}) => {
   )
 }
 
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
+}
